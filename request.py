0a36fb07fc15396ea6611b6c1bdd3434ad9c3db3
Molotov-141 <paul.lecornu08@gmail.com>
2026-01-06 10:35:56 +0100

    Code fonctionnel

--- request.py
+++ request.py
@@ -1,16 +1,26 @@
 import requests
 import markdown
 url = "https://api.openstreetmap.org/"
-response = requests.get(url)
-if response.status_code == 200:
+connexion = requests.get(url)
+if connexion.status_code == 200:
     print("Connexion réussie avec le serveur")
-elif response.status_code == 500:
+elif connexion.status_code == 500:
     print("Erreur 500 : Erreur interne du serveur")
-elif response.status_code == 204:
+elif connexion.status_code == 204:
     print("Aucune donnée à retourner")
+
+## id = "3649697385"
+## print("https://www.openstreetmap.org/api/0.6/node/3649697385.json")
 ## print(response.json)
-request = requests.get("https://api.openstreetmap.org/3649697385")
+
+## response = requests.get("https://www.openstreetmap.org/api/0.6/node/"+id+".json")
+## print("https://www.openstreetmap.org/api/0.6/node/"+id+".json")
 ## print(request.json)
-if request.status_code == 404:
-    print("Erreur 404 : Ressource non trouvée")
-    
+## if response.status_code == 404:
+    ## print("Erreur 404 : Ressource non trouvée")
+## print(response.json)
+id = "3649697385"
+api_url = "https://www.openstreetmap.org/api/0.6/node/3649697385.json"
+response = requests.get(api_url)
+donnees = response.json()
+print(donnees)
