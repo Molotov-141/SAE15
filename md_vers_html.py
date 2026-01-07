5fde7ffa8b411d86897b8da6d6369b8204303d16
Cst-saulieux <csaintsaulieux@gmail.com>
2026-01-07 09:37:45 +0200

    finalisation du script md -> html

--- md_vers_html.py
+++ md_vers_html.py
@@ -1,12 +1,14 @@
 import markdown
+import sys
 
-def md_to_html(fichier):
-    with open(fichier, 'r') as f: ## ouverture du fichier et extraction du contenu
+def convert(fichiermd, fichierhtml):
+    with open(fichiermd, 'r') as f: ## ouverture du fichier et extraction du contenu
         text = f.read()  ## stockage du contenu
 
     html = markdown.markdown(text) ## convertion md -> html
 
-    with open('a_def_aussi.html', 'w') as f: ## creation et ecriture dans un fichier en .html
+    with open(fichierhtml, 'w') as f: ## creation et ecriture dans un fichier en .html
         f.write(html)
+        print("fichier crée")
 
-md_to_html('a_def.md') ## execution de la fonction ( comme si un utilisateur ecrivais la commande dans un terminal )
+convert(sys.argv[1],sys.argv[2])
