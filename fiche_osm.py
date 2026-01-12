96be2d90ecac52accb502e6567593a9719f09c46
Molotov-141 <paul.lecornu08@gmail.com>
2026-01-12 13:38:05 +0100

    Test changement fichier md

--- fiche_osm.py
+++ fiche_osm.py
@@ -16,8 +16,7 @@ if connexion.status_code == 200:
     print("Connexion réussie avec le serveur")
 elif connexion.status_code == 500:
     print("Erreur 500 : Erreur interne du serveur")
-elif connexion.status_code == 204:
-    print("Aucune donnée à retourner")
+
 
 
 
@@ -27,6 +26,8 @@ response = requests.get(api_url)
 json_data = response.json() 
 if response.status_code == 404:
     print("Erreur 404 : Ressource non trouvée")
+elif connexion.status_code == 204:
+    print("Aucune donnée à retourner")
 #else:
 #    print(json_data)
     
