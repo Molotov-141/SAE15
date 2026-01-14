898c70d056eedc20f137cccf1901df0a089aac9d
Molotov-141 <paul.lecornu08@gmail.com>
2026-01-14 16:10:49 +0100

    datagrab.py with overpass API fonctionnel

--- datagrab.py
+++ datagrab.py
@@ -1,16 +1,44 @@
 import requests
 import time
-overpass_url = "http://overpass-api.de/api/interpreter"
+import os
+import markdown
+overpass_url0 = "http://overpass-api.de/api/interpreter"
+overpass_url1 = "https://lz4.overpass-api.de/api/interpreter"
 
 def get_dataset(query):
-    response = requests.get(overpass_url, params={'data': query})
-    json_data = response.json()
-    if response.status_code == 404:
-        print("Erreur 404 : Ressource non trouvée")
-    else:
-        return(json_data)
-    query = f"""
+    print(query)
+    request = f"""
     [out:json][timeout:25];
-    {{geocodeArea:{query}}}->.searchArea;
-    node["amenity"="]
+    area["name"="{query}"][admin_level=8]->.searchArea;
+    (
+    node["amenity"="bakery"](area.searchArea);
+    node["shop"="bakery"](area.searchArea);
+    );
+    out body;
     """
+    response = requests.get(overpass_url0, params={'data': request})
+    if response.status_code == 200:
+        print("Connexion au serveur établie, données récupérées.")
+        json_data = response.json()
+        return json_data
+    elif response.status_code == 404:
+        print("Erreur 404 : Ressource non trouvée")
+    elif response.status_code == 500:
+        print("Erreur 500 : Erreur interne du serveur")
+    elif response.status_code == 204:
+        print("Aucune donnée disponible pour cette requête.")
+    elif response.status_code == 400:
+        print("Erreur 400 : Requête invalide") 
+    elif response.status_code == 504:
+        response = requests.get(overpass_url1, params={'data': request})
+        if response.status_code == 200:
+            print("Connexion au serveur de secours établie, données récupérées.")
+            json_data = response.json()
+            return response.json()
+        else:
+            print(f"Erreur {response.status_code} : {response.text}")
+            return None
+    else:
+        print(f"Erreur {response.status_code} : {response.text}")
+        return None
+    
